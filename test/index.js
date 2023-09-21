9d14bdf353e223739eac440f46997bd5119ca6c1
Titus Wormer <tituswormer@gmail.com>
2023-09-21 11:03:54 +0200

    Change to use `exports`

--- test/index.js
+++ test/index.js
@@ -4,11 +4,11 @@ import process from 'node:process'
 import test from 'node:test'
 import {nameToEmoji} from 'gemoji'
 import {remark} from 'remark'
-import remarkGemoji from '../index.js'
+import remarkGemoji from 'remark-gemoji'
 
 test('remarkGemoji', async function (t) {
   await t.test('should expose the public api', async function () {
-    assert.deepEqual(Object.keys(await import('../index.js')).sort(), [
+    assert.deepEqual(Object.keys(await import('remark-gemoji')).sort(), [
       'default'
     ])
   })
